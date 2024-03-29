7d8275421299cba6ac5e42f084e045728dbde227
Michael Chavarria <michael93414@gmail.com>
2024-03-29 13:40:08 +0100

    scafolding realizato

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>{{$title ?? 'PortfoglioMike'}}</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Fonts -->
+        
+        
+
+        <!-- Styles -->
+        
+    </head>
+    <body >
+           <x-nav/>
+
+           <div class="min-vh-100 ">
+                {{$slot}}
+           </div>
+
+           <x-footer/>
+    </body>
+</html>
