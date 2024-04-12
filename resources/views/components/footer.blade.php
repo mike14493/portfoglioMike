ef9144cd9950388412685945120b5fbca480933c
Michael Chavarria <michael93414@gmail.com>
2024-04-12 15:04:21 +0200

    Portfolio finito

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,8 +2,8 @@
   <footer class="py-3 my-4 ">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3 ">
       <li class="nav-item"><a href="https://www.linkedin.com/in/darwin-michael-chavarr%C3%ADa-farf%C3%A1n/" class="nav-link px-2 footer Negro Social"><i class="fa-brands fa-linkedin fa-3x"></i></a></li>
-      <li class="nav-item"><a href="https://www.instagram.com/michael93414?igsh=MTViYTRwcHRnNTBwZA==" class="nav-link px-2 Negro Social mx-5"><i class="fa-brands fa-square-instagram fa-3x"></i></a></li>
-      <li class="nav-item"><a href="https://m.facebook.com/profile.php?id=100001124537032" class="nav-link px-2 Negro Social me-5"><i class="fa-brands fa-facebook fa-3x"></i></a></li>
+      <li class="nav-item"><a href="https://www.instagram.com/michael93414?igsh=MTViYTRwcHRnNTBwZA==" class="nav-link px-2 Negro Social mx-3 mx-md-5 "><i class="fa-brands fa-square-instagram fa-3x"></i></a></li>
+      <li class="nav-item"><a href="https://m.facebook.com/profile.php?id=100001124537032" class="nav-link px-2 Negro Social me-3 mx-md-5"><i class="fa-brands fa-facebook fa-3x"></i></a></li>
       <li class="nav-item"><a href="https://github.com/mike14493" class="nav-link px-2 Negro Social"><i class="fa-brands fa-square-github fa-3x"></i></a></li>
     </ul>
     <p class="text-center Negro">&copy; Michael Chavarria</p>
